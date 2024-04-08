67076d3b76b0a4b6c454eac7c8a3f8535d16af75
fardin-ahmed-1 <contactfardin22@gmail.com>
2024-04-08 09:48:53 +0600

    design shop page

--- src/pages/home/Trendyproducts.jsx
+++ src/pages/home/Trendyproducts.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Container from '../../shared/Container'
-import OutlineButton from '../../shared/OutlineButton'
-
+import { BsCart3 } from "react-icons/bs";
+import { IoHeartOutline } from "react-icons/io5";
+import { AiOutlineRetweet } from "react-icons/ai";
+import { LiaEyeSolid } from "react-icons/lia";
 const Trendyproducts = () => {
     const [products, setProducts] = useState([])
 
@@ -17,11 +19,27 @@ const Trendyproducts = () => {
             <Container>
                 <div className='grid grid-cols-4 gap-8'>
                     {
-                        products.map((product, index) => <div key={index}>
+                        products.map((product, index) => <div key={index}
+                            className='overflow-hidden'>
                             {/* Product feature image */}
-                            <div>
-                                <img className='rounded-md'
-                                    src={product.featureImg} alt="" />
+                            <div className='relative overflow-hidden bg-[#D9D9D9]'>
+                                <img className='rounded-md hover:scale-125 hover:rotate-6 w-full transition-all duration-300'
+                                    src={product.featureImg} alt="Feature image" />
+
+                                <ul className='flex items-center justify-between gap-6 px-6'>
+                                    <li className='p-4 bg-white hover:bg-primary'>
+                                        <BsCart3 className='text-3xl' />
+                                    </li>
+                                    <li className='p-4 bg-white hover:bg-primary'>
+                                        <IoHeartOutline className='text-3xl' />
+                                    </li>
+                                    <li className='p-2 bg-white hover:bg-primary'>
+                                        <AiOutlineRetweet />
+                                    </li>
+                                    <li className='p-2 bg-white hover:bg-primary'>
+                                        <IoHeartOutline />
+                                    </li>
+                                </ul>
                             </div>
                             {/* Product Information */}
                             <div className='my-5'>
@@ -32,11 +50,11 @@ const Trendyproducts = () => {
                                     <span className='text-xl font-normal line-through text-[#888888]'>${product.discount}</span>
                                 </div>
                                 {/* Add to cart button */}
-                              <div className='flex flex-col justify-center items-center mt-5'>
-                              <button className='py-2 px-10 lg:py-4 lg:px-12 outline-none border-primary border-[3px] text-xl font-medium text-heading hover:text-white bg-no hover:bg-primary hover:border-primary  duration-300 transition-all text-center'
+                                <div className='flex flex-col justify-center items-center mt-5'>
+                                    <button className='py-2 px-10 lg:py-4 lg:px-12 outline-none border-primary border-[3px] text-xl font-medium text-heading hover:text-white bg-no hover:bg-primary hover:border-primary  duration-300 transition-all text-center'
                                     // onClick={onclick} 
                                     >Add to Cart</button>
-                              </div>
+                                </div>
                             </div>
                         </div>)
                     }
