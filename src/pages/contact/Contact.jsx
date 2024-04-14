a3a0d19c732b393e6802662f3c2427d041d5b0fc
fardin-ahmed-1 <contactfardin22@gmail.com>
2024-04-14 11:35:44 +0600

    create contact us page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import PageHeader from '../../components/shared/PageHeader'
+import Container from '../../components/shared/Container'
+import ContactInfo from './ContactInfo'
+import ContactForm from './ContactForm'
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <PageHeader page='Contact Us'/>
+      <Container>
+        <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-7 gap-16 mt-16 items-center justify-between'>
+          <div className='md:col-span-2'>
+          <ContactInfo/>
+          </div>
+          <div className='md:col-span-5'>
+          <ContactForm/>
+          </div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
