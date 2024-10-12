7e6c56657afc4d5b95f8f8456318a5e0ef6a98bd
devfardin <contactfardin22@gmail.com>
2024-10-12 23:20:16 +0600

    create functin for login user and register user

--- frontend/src/providers/AuthProviders.jsx
+++ frontend/src/providers/AuthProviders.jsx
@@ -1,20 +1,48 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { firebaseApp } from "../firebase/firebase.config";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 const AuthContext = createContext(null);
 const AuthProviders = ({ children }) => {
   const [user, setUpser] = useState(null);
   const [loading, setLoading] = useState(true);
   const auth = getAuth(firebaseApp);
+
+  //   User Registration
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
   // User Login
   const LoginInUser = (email, password) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUpser(currentUser);
+      setLoading(false);
+    });
+    return () => unSubscribe();
+  }, []);
+
   const authInfo = {
     LoginInUser,
+    loading,
+    setLoading,
+    user,
+    createUser,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
