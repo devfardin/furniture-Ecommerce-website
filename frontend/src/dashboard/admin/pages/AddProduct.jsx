a7bc937b8388b83c55b3bdb33a850bef942b0dd0
devfardin <contactfardin22@gmail.com>
2024-10-28 12:34:15 +0600

    product upload page

--- frontend/src/dashboard/admin/pages/AddProduct.jsx
+++ frontend/src/dashboard/admin/pages/AddProduct.jsx
@@ -7,8 +7,10 @@ const AddProduct = () => {
         <h1 className="text-xl font-medium text-heading border-b border-dashBorder pb-2">
           Create Product
         </h1>
-        <div className="mt-4 flex flex-col gap-4">
-          <div className="flex flex-col gap-2.5">
+
+        <div className="mt-4 flex flex-col gap-5">
+          {/* Product Title */}
+          <div className="flex flex-col gap-2">
             <label
               className="text-lg font-semibold text-pera"
               htmlFor="Product-title"
@@ -20,9 +22,100 @@ const AddProduct = () => {
               name="productName"
               id="Product-title"
               placeholder="Enter Product Title"
-              className="py-3 px-4 border border-dashPrimary hover:border-primary outline-none w-full rounded-md text-lg font-normal"
+              className="py-3 px-4 border border-[#B1B5C3] hover:border-dashPrimary focus:border-dashPrimary outline-none w-full rounded-md text-lg font-normal"
             />
           </div>
+        {/* Product Description */}
+          <div className="flex flex-col gap-2">
+            <label
+              className="text-lg font-semibold text-pera"
+              htmlFor="Product-description"
+            >
+              Product Description
+            </label>
+            <textarea
+            rows='4'
+              type="text"
+              name="productDescription"
+              id="Product-description"
+              placeholder="Enter Product Description"
+              className="py-3 px-4 border border-[#B1B5C3] hover:border-dashPrimary focus:border-dashPrimary outline-none w-full rounded-md text-lg font-normal"
+            />
+          </div>
+
+          {/* Product Price and Discount */}
+          <div className="flex items-center justify-between gap-5">
+            {/* Product Price */}
+          <div className="flex flex-col flex-1 gap-2">
+            <label
+              className="text-lg font-semibold text-pera"
+              htmlFor="Product-price"
+            >
+             Product Price
+            </label>
+            <input
+              type="number"
+              name="productPrice"
+              id="Product-price"
+              placeholder="Enter Product Price"
+              className="py-3 px-4 border border-[#B1B5C3] hover:border-dashPrimary focus:border-dashPrimary outline-none w-full rounded-md text-lg font-normal"
+            />
+          </div>
+
+          <div className="flex flex-col flex-1 gap-2">
+            <label
+              className="text-lg font-semibold text-pera"
+              htmlFor="Product-discount"
+            >
+              Discount
+            </label>
+            <input
+              type="number"
+              name="productName"
+              id="Product-discount"
+              placeholder="Enter Product Discount"
+              className="py-3 px-4 border border-[#B1B5C3] hover:border-dashPrimary focus:border-dashPrimary outline-none w-full rounded-md text-lg font-normal"
+            />
+          </div>
+          </div>
+
+          {/* Product Stock and SKU Number */}
+          <div className="flex items-center justify-between gap-5">
+            {/* Product Price */}
+          <div className="flex flex-col flex-1 gap-2">
+            <label
+              className="text-lg font-semibold text-pera"
+              htmlFor="Product-stocks"
+            >
+             Product In Stocks
+            </label>
+            <input
+              type="number"
+              name="productStocks"
+              id="Product-stocks"
+              placeholder="Enter Product Stocks"
+              className="py-3 px-4 border border-[#B1B5C3] hover:border-dashPrimary focus:border-dashPrimary outline-none w-full rounded-md text-lg font-normal"
+            />
+          </div>
+
+          <div className="flex flex-col flex-1 gap-2">
+            <label
+              className="text-lg font-semibold text-pera"
+              htmlFor="Product-sku"
+            >
+             Product Sku 
+            </label>
+            <input
+              type="number"
+              name="productSku"
+              id="Product-sku"
+              placeholder="Enter Product Sku"
+              className="py-3 px-4 border border-[#B1B5C3] hover:border-dashPrimary focus:border-dashPrimary outline-none w-full rounded-md text-lg font-normal"
+            />
+          </div>
+          </div>
+
+
         </div>
       </div>
     </form>
